5b8cea7219f0a727d4b9ad9d6649733865a0ddd7
Zhanbo <13489103779@163.com>
2023-03-29 07:55:08 -0400

    Enemy Model

--- Assets/Scripts/Modules/RightWing.cs
+++ Assets/Scripts/Modules/RightWing.cs
@@ -1,7 +1,15 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RightWing : Module
 {
+    private ParticleSystem[] _engineVFX;
+    private void Awake()
+    {
+        _engineVFX = gameObject.GetComponents<ParticleSystem>();
+    }
+
     public override void Action()
     {
         ship.transform.Rotate(Vector3.back * constant * Time.deltaTime);
